9c3f683e60b047d083afdd36e3e8925e93e90d8e
Vladimir <vladamata21@gmail.com>
2021-12-22 20:54:32 +0100

    initiall

---
+++ src/components/Charts/Doughnut2d.js
@@ -0,0 +1,29 @@
+import React from "react";
+import ReactFC from "react-fusioncharts";
+import FusionCharts from "fusioncharts";
+import Column2D from "fusioncharts/fusioncharts.charts";
+import FusionTheme from "fusioncharts/themes/fusioncharts.theme.candy";
+
+ReactFC.fcRoot(FusionCharts, Column2D, FusionTheme);
+
+const ChartComponent = ({ data }) => {
+  const chartConfigs = {
+    type: "doughnut2d",
+    width: "100%",
+    height: "400",
+    dataFormat: "json",
+    dataSource: {
+      chart: {
+        caption: "Stars Per Language",
+        theme: "candy",
+        decimals: 0,
+        doughnutRadius: "45%",
+        showPercentValues: 0,
+      },
+      data,
+    },
+  };
+  return <ReactFC {...chartConfigs} />;
+};
+
+export default ChartComponent;
---
+++ src/components/Charts/Pie3D.js
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactFC from "react-fusioncharts";
+import FusionCharts from "fusioncharts";
+import Column2D from "fusioncharts/fusioncharts.charts";
+import FusionTheme from "fusioncharts/themes/fusioncharts.theme.fusion";
+
+ReactFC.fcRoot(FusionCharts, Column2D, FusionTheme);
+
+const ChartComponent = ({ data }) => {
+  const chartConfigs = {
+    type: "pie3d",
+    width: "100%",
+    height: "400",
+    dataFormat: "json",
+    dataSource: {
+      chart: {
+        caption: "Languages",
+        theme: "fusion",
+        decimals: 0,
+        pieRadius: "45%",
+      },
+      data,
+    },
+  };
+  return <ReactFC {...chartConfigs} />;
+};
+
+export default ChartComponent;
